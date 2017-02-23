c56e0b29722aba17e5945441d0a458288a6edcb1
ab-rasel <abrasel600@gmail.com>
2017-02-23 12:11:48 +0600

    ok this file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,49 @@
 $(document).ready(function() {
+ /*MobileMenu Start By Shagor */
+    $('.scrolls a').on('click', function () {
+        $('html, body').animate({scrollTop: $(this.hash).offset().top - (0)}, 1000);
+        return false;
+    });
 
-    
+    function Scroll() {
+
+        var contentTop = [];
+        var contentBottom = [];
+        var winTop = $(window).scrollTop();
+        var rangeTop = 200;
+        var rangeBottom = 500;
+
+        $('.mainFeatureMenu').find('.scrolls > a').each(function () {
+            var atr = $(this).attr('href');
+            if ($(atr).length > 0)
+            {
+                contentTop.push($($(this).attr('href')).offset().top(100));
+                contentBottom.push($($(this).attr('href')).offset().top + $($(this).attr('href')).height());
+            }
+
+        });
+
+        $.each(contentTop, function (i) {
+
+            if (winTop > contentTop[i] - rangeTop) {
+
+                $('.mainFeatureMenu li.scrolls')
+                        .removeClass('active')
+                        .eq(i).addClass('active');
+            }
+        });
+
+    }
+    /* feature menu active*/
+      $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 3500)
+        {
+            $(".featureMenu").addClass('featureMenufixed');
+        } else
+        {
+            $(".featureMenu").removeClass('featureMenufixed');
+        }
+    });
+    /* feature menu active*/
 });
 

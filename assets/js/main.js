ccd9e03f54d66954c4686650bb492f836ff7d6f5
rasel <abrasel600@gmail.com>
2017-03-07 14:49:52 +0600

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,7 +102,17 @@ $(document).ready(function () {
         $(".userBox").toggle('slow');
     });
 
-
+    
+      $('.collapse.in').prev('.panel-heading').addClass('active');
+  $('#accordion, #bs-collapse')
+    .on('show.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').removeClass('active');
+    });
+    
+    
 });
 
     
